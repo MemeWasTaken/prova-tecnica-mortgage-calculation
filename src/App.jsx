0d536a71ffbc38197fc3d2236355598e4ee9afc1
LazzariDev <lazzaridev@gmail.com>
2026-09-22 15:39:02 +0200

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div className="flex min-h-screen flex-col bg-gray-50">
       <Navbar />
       <Main />
+      <Footer />
     </div>
   );
 }
